a941c7904ef40972f6286a86459d728e6f200401
ObradovicNikola <obradovicnikola009@gmail.com>
2019-09-26 21:59:49 +0200

    Initial commit

--- src/components/Redux.js
+++ src/components/Redux.js
@@ -1,61 +1,95 @@
 import React from 'react'
-import { createStore } from 'redux'
-
+import { createStore, combineReducers } from 'redux'
+import uuid from 'uuid'
 
 
 export default function Redux() {
-    const store = createStore((state = { count: 0 }, action) => {
+    const demoState = {
+        expenses: [{
+            id: 'pasgfassg',
+            description: 'rent nov',
+            note: 'important',
+            amount: 300,
+            createdAt: 0
+        }],
+        filters: {
+            text: 'rent',
+            sortBy: 'amount', //date or amount
+            startDate: undefined,
+            endDate: undefined
+        }
+    }
+
+    // Expenses reducer
+    const addExpense = (
+        {
+            description = '',
+            note = '',
+            amount = 0,
+            createdAt = 0
+        } = {}
+    ) => ({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: uuid(),
+            description,
+            note,
+            amount,
+            createdAt
+        }
+    })
+
+    const expensesReducerDefaultState = []
+
+    const expensesReducer = (state = expensesReducerDefaultState, action) => {
         switch (action.type) {
-            case 'INCREMENT':
-                return {
-                    count: state.count + (action.incrementBy ? action.incrementBy : 1)
-                }
-            case 'DECREMENT':
-                return {
-                    count: state.count - (action.decrementBy ? action.decrementBy : 1)
-                }
-            case 'RESET':
-                return {
-                    count: 0,
-                }
+            case 'ADD_EXPENSE':
+                return state.concat(action.expense)
             default:
                 return state
         }
+    }
+
+    const filtersReducerDefaultState = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+
+    }
+
+    const filtersReducer = (state = filtersReducerDefaultState, action) => {
+        switch (action.type) {
+
+            default:
+                return state
+        }
+    }
+
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer
+        })
+    )
+
+    store.subscribe(() => {
+        console.log(store.getState())
     })
+
+    store.dispatch(addExpense({ description: 'Rent', amount: 300 }))
 
     // called every time state changes
     const unsubscribe = store.subscribe(() => {
         console.log(store.getState())
     })
 
-    store.dispatch({
-        type: 'INCREMENT',
-        incrementBy: 5
-    })
-
     // call to stop executing subscribe code on changes
-    // unsubscribe()
-
-    store.dispatch({
-        type: 'INCREMENT'
-    })
-
-    store.dispatch({
-        type: 'RESET'
-    })
-
-    store.dispatch({
-        type: 'DECREMENT'
-    })
-
-    store.dispatch({
-        type: 'DECREMENT',
-        decrementBy: 3
-    })
+    unsubscribe()
 
     return (
         <div>
-            <p>Store:</p>
+            <p>Store: </p>
         </div>
     )
 }
