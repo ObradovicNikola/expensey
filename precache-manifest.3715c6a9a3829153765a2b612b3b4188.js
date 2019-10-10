906f4d1f7ebd1329a2a51c1bcf49a9f024abbfec
ObradovicNikola <obradovicnikola009@gmail.com>
2019-10-10 11:00:01 +0200

    Updates

---
+++ precache-manifest.3715c6a9a3829153765a2b612b3b4188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5332aa77078776a61a2ce61ecc2c0e5b",
+    "url": "/expensey/index.html"
+  },
+  {
+    "revision": "cc3d81884e55fef8cb05",
+    "url": "/expensey/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "334572938920b5eb24d3",
+    "url": "/expensey/static/css/main.7eeb82bf.chunk.css"
+  },
+  {
+    "revision": "cc3d81884e55fef8cb05",
+    "url": "/expensey/static/js/2.81914cf0.chunk.js"
+  },
+  {
+    "revision": "334572938920b5eb24d3",
+    "url": "/expensey/static/js/main.53604845.chunk.js"
+  },
+  {
+    "revision": "b096b762b08fc1a05641",
+    "url": "/expensey/static/js/runtime-main.f3755aa8.js"
+  }
+]);
